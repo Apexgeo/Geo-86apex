5265f531a415a649dc2e03700821d4607f76a6c5
Apexgeo <134904196+Apexgeo@users.noreply.github.com>
2024-05-04 14:03:13 -0600

    y

--- Mate_basica.c
+++ Mate_basica.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 int main(){
-    float suma,multi,divi,a,b;
+    float suma,resta,multi,divi,a,b;
     printf("Ingrese el primer numero:");
     scanf("%f",&a);
     printf("ingrese el segundo numero:");
     scanf("%f",&b);
     
     suma=a+b;
+    resta=a-b;
     multi=a*b;
     divi=a/b,
     
     printf("\nla suma:%.2f",suma);
+    printf("\nla resta:%.2f",resta);
     printf("\nla multiplicacion:%.2f",multi);
     printf("\nla division:%.2f",divi);
 }
