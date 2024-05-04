bd6c57a23a5b2baf519b2382dc3085b5fce902d8
Apexgeo <134904196+Apexgeo@users.noreply.github.com>
2024-05-04 13:42:51 -0600

    ya echo

---
+++ hola.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    float suma,multi,divi,a,b;
+    printf("Ingrese el primer numero:");
+    scanf("%f",&a);
+    printf("ingrese el segundo numero:");
+    scanf("%f",&b);
+    
+    suma=a+b;
+    multi=a*b;
+    divi=a/b,
+    
+    printf("\nla suma:%.2f",suma);
+    printf("\nla multiplicacion:%.2f",multi);
+    printf("\nla division:%.2f",divi);
+}
